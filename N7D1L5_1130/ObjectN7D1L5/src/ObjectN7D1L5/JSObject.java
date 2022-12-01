90429d29eba311428eed26903ee0c75a17ed7bc4
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2022-12-01 16:21:01 +0100

    pótlás

---
+++ N7D1L5_1130/ObjectN7D1L5/src/ObjectN7D1L5/JSObject.java
@@ -0,0 +1,11 @@
+package ObjectN7D1L5;
+
+public class JSObject {
+
+    public void put(String string, String string2) {
+    }
+
+    public void put(String string, int i) {
+    }
+
+}
---
+++ N7D1L5_1130/ObjectN7D1L5/src/ObjectN7D1L5/ObjectN7D1L5.java
@@ -0,0 +1,14 @@
+package ObjectN7D1L5;
+
+public class ObjectN7D1L5 {
+    public static void main(String[] args) {
+        JSObject obj = new JSObject();
+
+        obj.put("nev", "Korom Tamas");
+        obj.put("neptun", "N7D1L5");
+        obj.put("szak", "Progterv");
+
+        System.out.print(obj);
+
+    }
+}
